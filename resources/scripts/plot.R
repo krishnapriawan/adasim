733adcf3937034b9cb0ab9b1dd2cdbdbac89b112
unknown <krisha.priawan@gmail.com>
2019-07-27 20:27:24 +0900

    modified visualization

--- resources/scripts/plot.R
+++ resources/scripts/plot.R
@@ -1,9 +1,10 @@
 
 map_s <- function(str) {
-	if ( str == "adasim.algorithm.LookaheadShortestPathCarStrategy" ) {
-		"ShortestPath"
+	print(str)
+	if ( is.na(str) || str == "adasim.algorithm.routing.AdaptiveRoutingAlgorithm" ) {
+		"TrafficLookaheadRouting"
 	} else {
-		"TrafficLookahead"
+		"PriorityRouting"
 	}
 }
 
@@ -11,16 +12,17 @@ plot_data <- function(file) {
 	data <- read.table( paste(file, "csv", sep="."), header=T, sep=',')
 	p <- tapply( data$Time, list(data$Hops, data$Strategy) , mean)
 	l <- levels(data$Strategy)
+	print(l[1])
 	pdf( paste(file, "pdf", sep=".") )
-	barplot(t(p), beside=T, legend= c( map_s(l[1]) , map_s(l[2]) ))
+	barplot(t(p), beside=T, legend= c( map_s(l[1]) , map_s(l[2]) ), args.legend = list(x ='topright', bty='n', inset=c(-0.25,0)))
 	dev.off()
 }
 
 plot_all_data <- function() {
-	files <- list.files( path=".", pattern=".+.csv$" )
+	files <- list.files( path=".", pattern=".+.csv$" ) 
 	for ( file in files) {
 		s <- strsplit( file, ".csv" )
-		plot_data(s)
+		plot_time_data(s)
 	}
 }
 
@@ -29,6 +31,10 @@ plot_time_data <- function(file) {
 	p <- tapply( data$Time, list(data$Cars, data$Strategy) , mean)
 	l <- levels(data$Strategy)
 	pdf( paste(file, "pdf", sep=".") )
-	barplot(t(p), beside=T, legend= c( map_s(l[1]) , map_s(l[2]) ), xlab ="# of vehicles", ylab = "mean travel time", args.legend = list( x = "topleft" ) )
+	barplot(t(p), beside=T, 
+		legend= c( map_s(l[1]) , map_s(l[2]) ), 
+		xlab ="# of vehicles", 
+		ylab = "mean travel time", 
+		args.legend = list( x = "topleft", bty='n' ) )
 	dev.off()
-}
+} 
